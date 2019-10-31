598c9b91ef3f06352786baf72bf2259d5b6b9f2c
Lookuz <cweyyeh512@gmail.com>
2019-10-31 22:30:09 +0800

    Updated window-based detection and non-max suppression

--- BagOfWords.py
+++ BagOfWords.py
@@ -8,12 +8,14 @@ import cyvlfeat as vlfeat
 
 from scipy.spatial.distance import cdist
 
+from SlidingWindow import *
+
 KAZE_DESC_SIZE = 64
 
 # Function that extracts features as descriptors from the given image 
 # TODO: Currently uses KAZE descriptor. To add SIFT/SURF descriptor functionality
+# Set the limit parameter to false to take the full range of descriptors in the image
 def extract_feature(image, vector_size=64, limit=True):
-
     kaze = cv2.KAZE_create()
     # Identify keypoints
     keypoints = kaze.detect(image)
@@ -73,3 +75,47 @@ def extract_histograms(images, bag_of_words, metric='euclidean'):
         histograms.append(histogram)
 
     return np.array(histograms)
+
+# Functions that detects Waldo in the given image using window-based techniques
+# Performs a sliding window over each window on the image with size specified by window_size,
+# and scores each window using the model supplied
+# Pyramidal scaling is also applied to apply sliding window over multiscale situations
+# window_size = (r, c)/ (y, x)
+def detect(image, bag_of_words, clf, step_size=250, window_size=(400, 200), scale=1.5, pyramid_window=(2000, 2000)):
+    detections = [] # To store detected window coordinates
+    current_scale = 0
+    
+    # Apply pyramidal sliding window
+    for scaled_image in image_pyramid(image, scale=scale, minSize=pyramid_window):
+        # Resized image too small
+        if scaled_image.shape[0] < pyramid_window[1] or scaled_image.shape[1] < pyramid_window[0]:
+            break
+        
+        for (coordinates, window) in sliding_window(scaled_image, step_size=step_size, window_size=window_size):
+            # if window.shape[0] != window_size[0] or window.shape[1] != window_size[1]:
+            #         continue
+            y, x, y_end, x_end = coordinates
+
+            # For this window, extract the descriptors then extract the histogram vector from descriptors
+            # _, descriptors = extract_feature(window, limit=False)
+            # feature_vector = extract_histogram(descriptors, bag_of_words)
+            feature_vector = extract_histograms([window], bag_of_words)
+            # Predict Waldo
+            prediction = clf.predict(feature_vector)[0]
+            if prediction == 1:
+                # Rescale coordinates
+                win_x = int(x * (scale ** current_scale))
+                win_y = int(y * (scale ** current_scale))
+                win_x_end = win_x + int(window_size[1] * (scale ** current_scale))
+                win_x_end = min(win_x_end, image.shape[1])
+                win_y_end = win_y + int(window_size[0] * (scale ** current_scale))
+                win_y_end = min(win_y_end, image.shape[0])
+                # Add bounding box
+                detections.append((win_x, win_y, win_x_end, win_y_end))
+                
+        current_scale += 1
+    
+    # Perform Non-Maximum Suppression
+    detections = non_max_suppression(np.array(detections), threshold=0.5)
+    
+    return detections
--- SlidingWindow.py
+++ SlidingWindow.py
@@ -1,13 +1,14 @@
 import argparse
 import cv2
 import imutils
+import numpy as np
 
 # Creates an image pyramid using the scale provided
 def image_pyramid(image, scale=1.5, minSize=(100, 100)):
     # Original image
     yield image
 
-    while (image.shape[0] < minSize[0]) and (image.shape[1] < minSize[1]):
+    while (image.shape[0] < minSize[1]) and (image.shape[1] < minSize[0]):
         width = int(image.shape[1] / scale)
         image = imutils.resize(image, width=width)
 
@@ -28,5 +29,52 @@ def sliding_window(image, step_size, window_size):
             x_end = min(max_c, x + window_size[1])
             # coordinates of current window
             coordinates = (y, x, y_end, x_end)
-            yield (coordinates, image[y:y_end, x:x_end])
+            yield (coordinates, image[y:y_end, x:x_end, :])
 
+
+# Function that applies non maximum suppression to 
+# the list of bounding boxes to remove overlapping windows 
+# threshold parameter determines area of overlap given to
+# windows before they are suppressed
+# TODO: Functionality may need to be refined
+def non_max_suppression(boxes, threshold=0.5):
+    # No detections
+    if boxes is None or len(boxes) == 0:
+        return []
+    
+    # Convert from integer to float coordinates to support division operation
+    if boxes.dtype.kind == 'int':
+        boxes = boxes.astype('float')
+        
+    final_windows = []
+    
+    x = boxes[:, 0]
+    y = boxes[:, 1]
+    x_end = boxes[:, 2]
+    y_end = boxes[:, 3]
+    area = (x_end - x + 1) * (y_end - y + 1)
+    index = np.argsort(y_end) # Sort by lower right coordinate
+    
+    while len(index) > 0:
+        # Grab last element as comparison
+        current = index[len(index) - 1]
+        final_windows.append(current)
+        
+        # Get x, y coordinates for intersection
+        # Upper boxes
+        x_max = np.maximum(x[current], x[index[:len(index) - 1]])
+        y_max = np.maximum(y[current], y[index[:len(index) - 1]])
+        x_min = np.maximum(x_end[current], x_end[index[:len(index) - 1]])
+        y_min = np.maximum(y_end[current], y_end[index[:len(index) - 1]])
+        
+        # Compute differences as the width and height of intersected box
+        width = np.maximum(0, x_min - x_max + 1)
+        height = np.maximum(0, y_min - y_max + 1)
+        
+        # Calculate intersection ratio
+        intersection = (width * height) / area[index[:len(index) - 1]]
+        
+        # Remove boxes with intersection ratio over the threshold and last box
+        index = np.delete(index, np.concatenate(([len(index) - 1], np.where(intersection > threshold)[0])))
+    
+    return boxes[final_windows].astype('int')
